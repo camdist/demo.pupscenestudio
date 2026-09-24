509877acd7edd71c0fb4c88b4ffaa41c497152d6
camdist <campodigitalstudio@gmail.com>
2026-09-24 10:56:59 +0800

    Add files via upload

---
+++ assets/demo-config.js
@@ -0,0 +1,7 @@
+window.PUPSCENE_DEMO_CONFIG = {
+  // Change this only if your Sales Funnel checkout is hosted somewhere else.
+  checkoutUrl: 'https://pupscenestudio.site/payment.html',
+  // Same-origin endpoint included as a Cloudflare/D1 scaffold in this package.
+  eventEndpoint: '/api/demo-event',
+  demoLimit: 10
+};
